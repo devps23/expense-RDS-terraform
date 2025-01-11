7761f3e7f6f3a0942d7117aadfc2c6cbcb3bf457
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-11 23:28:12 +0530

    identifier name in rds

--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -12,6 +12,7 @@ resource "aws_db_instance" "db_instance" {
   publicly_accessible  = false
   vpc_security_group_ids = [aws_security_group.rds.id]
   db_subnet_group_name = aws_db_subnet_group.subnet_group.name
+  identifier           = "rds-dev"
 }
 
 
