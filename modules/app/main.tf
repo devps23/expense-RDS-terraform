755312b2843db86f27870ce0793779247c16f19a
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-01-12 20:11:37 +0530

    for every instance id changed null resources will not be execute,so we use triggers if there is any change in an instance

--- modules/app/main.tf
+++ modules/app/main.tf
@@ -29,6 +29,9 @@ resource "aws_instance" "instance" {
 
 
 resource "null_resource" "null_instance" {
+  triggers = {
+    instance = aws_instance.instance.id
+  }
   connection {
     type     = "ssh"
     user     = jsondecode(data.vault_generic_secret.my_secret.data_json).username
